1107948ce20aa126bf614cf2c5eadc61743a118f
Chankyung Pak <paniked83@gmail.com>
2019-04-26 18:41:56 +0200

    Finish scatching

--- pull_users.py
+++ pull_users.py
@@ -1,18 +1,36 @@
 import twitter_tokens
 import csv
+import time
+import subprocess
 from mySQL_connector import *
 
 conn = create_cnx()
 c = create_cursor(conn)
 
-i = 1
+i = 0
 while True:
-    c.execute("SELECT user FROM database WHERE num = {}".format(str(i)))
-    
-    screen_name = c[0][0]
+    i += 15
+    c.execute("SELECT user FROM database WHERE num >=  {} AND num <= {}".format(str(i-14), str(i)))
+         
+    screen_names = [row[0] for row in c]
 
-    send_it_to Twitte here!
+    with open("retweeter.csv") as f:
+        retweeters = f.csv_write()
 
-    if exceed limits, stop! import error from twwweee
+    # Look up retweeter.csv here
+
+    screen_names = [retweeter in retweeter for screen_names if retweeter not in retweeters]
+
+    # Get ideology from R code.  
+    subprocess.call("Rscript --vanilla eval_ideo.R {}".format("("+screen_name+")". shell=True)
+
+    # Append new retweeters -> maybe add it as JSON (with ideology info)
+    retweeters.append(screen_names)
+    with open("reteeter.csv") as f:
+       # Append here (not above)
+ 
+    time.sleep(15 * 60)
+       
+    #if exceed limits, stop! import error from twwweee
     
-    
+c.disconnect()
