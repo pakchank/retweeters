252b3e5e34088446ba042abf29c7c50092a02c8d
Chankyung Pak <paniked83@gmail.com>
2019-04-29 11:56:22 +0200

    Update both

--- eval_ideo.R
+++ eval_ideo.R
@@ -1,17 +1,32 @@
 #!/usr/bin/env Rscript
 
-#args = commandArgs(trailingOnly=TRUE)
+users = commandArgs(trailingOnly=TRUE)
 
-#if (length(args) != 1) {
-#  stop("One argument must be supplied (input file).n", call.=FALSE)
-#} 
-
+if (length(args) == 0) {
+  stop("At least one user should be provided as an argument.", call.=FALSE)
+} else if (length(args) > 15) {
+  stop("Twitter API can't handle more than 15 users.")
+}
+    
 library(tweetscores)
 
-user <- "p_barbera"
-friends <- getFriends(screen_name=user, oauth="~/retweeters")
+estimates <- list()
 
-results <- estimateIdeology(user, friends)
+print(users)
 
-summary(results)
+for (user in users){
+    cat(paste("Fetching the friend list of", user))
+    cat("\n")
+    friends <- getFriends(screen_name=user, oauth="~/retweeters")
+
+    cat(paste("Measuring ideology of", user))
+    cat("\n")
+    results <- estimateIdeology(user, friends)
+
+    estimates[[user]] <- results
+
+    summary(results)
+}
+
+# How to return...?
 
--- pull_users.py
+++ pull_users.py
@@ -1,8 +1,9 @@
-import twitter_tokens
+#import twitter_tokens
 import csv
 import time
 import subprocess
 from mySQL_connector import *
+from pathlib import Path
 
 conn = create_cnx()
 c = create_cursor(conn)
@@ -10,28 +11,39 @@ c = create_cursor(conn)
 i = 0
 while True:
     i += 15
-    c.execute("SELECT user FROM database WHERE num >=  {} AND num <= {}".format(str(i-14), str(i)))
+    c.execute("SELECT user_name FROM xudong LIMIT 15")
+    #c.execute("SELECT user FROM database WHERE num >=  {} AND num <= {}".format(str(i-14), str(i)))
          
     screen_names = [row[0] for row in c]
 
-    with open("retweeter.csv", mode='r') as f: 
-        retweeters = [row.split()[0] for row in f]
+    retweeter_list = Path("retweeter.csv")
+    
+    if retweeter_list.is_file():
+        with open(retweeter_list, mode='r') as f: 
+            retweeters = [row.split()[0] for row in f]
+    else:
+        retweeters = list()
 
     # Look up retweeter.csv here
 
-    screen_names = [retweeter in retweeter for screen_names if retweeter not in retweeters]
+    screen_names = [retweeter for retweeter in  screen_names if retweeter not in retweeters]
 
     # Get ideology from R code.  
-    subprocess.call("Rscript --vanilla eval_ideo.R {}".format("("+screen_name+")". shell=True)
+    print("Rscript --vanilla eval_ideo.R {}".format(" ".join(screen_names)))
+    subprocess.call("Rscript --vanilla eval_ideo.R {}".format(" ".join(screen_names)), shell=True)
+    # Should be something like "x = subprocess.check_output(cmd, universal_newlines=True)
+    
     ## Then, how do I get the results? as dic {"users_name": "score", ...}
     
-    dict_score = dict()
-    for user, score in some_results.items():
-        dict_score[user] = score
+    #dict_score = dict()
+    #for user, score in some_results.items():
+    #    dict_score[user] = score
 
-    with open("reteeter.csv", mode='a') as f:
-        for user, score in dict_score.items():
-            f.write(user, score)
+    #with open("reteeter.csv", mode='a') as f:
+    #    for user, score in dict_score.items():
+    #        f.write(user, score)
+
+    print(screen_names)
 
     time.sleep(15 * 60)
        
